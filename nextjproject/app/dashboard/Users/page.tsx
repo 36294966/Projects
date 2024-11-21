a7c0bb67da8de0b5cf157127078528e2b62be3b5
36294966 <erastusngamau8@gmail.com>
2024-11-21 23:09:37 +0200

    refactored

---
+++ nextjproject/app/dashboard/Users/page.tsx
@@ -0,0 +1,6 @@
+const page = () =>{
+    return (
+        <div>Users</div>
+    )
+    }
+    export default page
---
+++ nextjproject/app/dashboard/analytics/page.tsx
@@ -0,0 +1,6 @@
+const page = () =>{
+return (
+    <div>analytics</div>
+)
+}
+export default page
